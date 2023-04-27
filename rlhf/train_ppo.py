e671c14b31a1377c1f47b31b2bae4b7bf4e0e201
Alexey Gorbatovski <porsh-5@yandex.ru>
2023-04-28 00:28:30 +0300

    Added global epoches, requirements updated

--- rlhf/train_ppo.py
+++ rlhf/train_ppo.py
@@ -1,13 +1,16 @@
 import torch
 from tqdm import tqdm
+import wandb
+
 tqdm.pandas()
 
 from transformers import AutoTokenizer
-
 from trl import PPOTrainer, PPOConfig, AutoModelForCausalLMWithValueHead, set_seed
+
 from data.data_utils import build_dataset, collator
-from reward_pipelines.regression_reward import Reward_pipeline
 from training_utils.freeze import freeze_model
+from training_utils.checkpoint import save_checkpoint
+from reward_pipelines.regression_reward import Reward_pipeline
 
 import yaml
 from yaml import CLoader
@@ -25,8 +28,18 @@ def main(config_file):
     config = PPOConfig(**args_config['ppo_config'])
 
     data_config = args_config['data_config']
-
+    freeze_config = args_config['freeze_config']
     reward_config = args_config['reward_config']
+    save_config = args_config['save_config']
+
+    generation_kwargs = args_config['generation_config']
+
+    
+    config.data_config = data_config
+    config.freeze_config = freeze_config
+    config.save_config = save_config
+    config.generation_kwargs = config
+    config.reward_config = reward_config
 
     set_seed(config.seed)
 
@@ -35,9 +48,10 @@ def main(config_file):
     model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
     ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
     tokenizer = AutoTokenizer.from_pretrained(config.model_name)
-    tokenizer.pad_token = tokenizer.eos_token
 
-    model = freeze_model(model, args_config['freeze_config'])
+    model = freeze_model(model, freeze_config)
+
+    tokenizer.pad_token = tokenizer.eos_token
 
     ppo_trainer = PPOTrainer(config, model, ref_model, tokenizer, dataset=dataset, data_collator=collator, num_shared_layers=None)
 
@@ -47,14 +61,12 @@ def main(config_file):
 
     reward_pipe = Reward_pipeline(reward_config['model_name'], device)
 
-    generation_kwargs = {
-        "min_length": -1,
-        "top_k": 0.0,
-        "top_p": 1.0,
-        "do_sample": True,
-        "pad_token_id": tokenizer.eos_token_id,
-        "max_new_tokens": 512,
-    }
+    
+    run = wandb.init(reinit=False)
+
+    generation_kwargs["pad_token_id"] = tokenizer.eos_token_id
+    best_reward = -100
+
 
     for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
         query_tensors = batch["input_ids"]
@@ -63,11 +75,29 @@ def main(config_file):
         for query in query_tensors:
             response = ppo_trainer.generate(query, **generation_kwargs)
             response_tensors.append(response.squeeze())
-        batch["question_answer"] = tokenizer.batch_decode(response_tensors, skip_special_tokens=True)
+        # batch["question_answer"] = tokenizer.batch_decode(response_tensors, skip_special_tokens=True)
+        # batch["query"] = [tokenizer.decode(query_idx, skip_special_tokens=True) for query_idx in batch["input_ids"]]
+        batch["response"] = [tokenizer.decode(r.squeeze()[len(query_idx):], skip_special_tokens=True) for r, query_idx in zip(response_tensors, batch["input_ids"])]
 
-        rewards = reward_pipe(batch["question_answer"], reward_config['batch_size'])
+        #### Compute sentiment score
+        texts = [q + r for q, r in zip(batch["query"], batch["response"])]
+        rewards = reward_pipe(texts, reward_config['batch_size'])
+
+        #### Run PPO step
         stats = ppo_trainer.step(query_tensors, response_tensors, rewards)
         ppo_trainer.log_stats(stats, batch, rewards)
+
+        mean_reward = torch.mean(torch.tensor(rewards))
+
+        if (epoch + 1) % save_config['save_interval'] == 0:
+            save_checkpoint(ppo_trainer.model, run, epoch, mean_reward, save_config['checkpoint_dir'], 'ppo_checkpoint')
+
+        if mean_reward > best_reward:
+            save_checkpoint(ppo_trainer.model, run, epoch, mean_reward, save_config['checkpoint_dir'], 'max_reward_ppo')
+
+            best_reward = mean_reward
+
+    save_checkpoint(ppo_trainer.model, run, epoch, mean_reward, save_config['checkpoint_dir'], 'last_checkpoint')
     
     ppo_trainer.model.push_to_hub(args_config['hf_hub_name'])
 
--- rlhf/training_utils/checkpoint.py
+++ rlhf/training_utils/checkpoint.py
@@ -1,10 +1,10 @@
 from os.path import join as opj
 import wandb
 
-def save_checkpoint(model, run, epoch, reward, checkpoint_dir, name):
-    checkpoint_name = f'{name}-_reward-{reward}_epoch-{epoch}'
+def save_checkpoint(model, run, global_epo, epoch, reward, checkpoint_dir, name):
+    checkpoint_name = f'{name}-glob_epo_{global_epo}-reward_{round(reward.item(), 2)}-epoch_{epoch}'
     model.save_pretrained(opj(checkpoint_dir, checkpoint_name))
 
-    artifact = wandb.Artifact(f'checkpoint_name', type='model', metadata={'mean_reward': reward, 'epoch': epoch})
+    artifact = wandb.Artifact(name, type='model', metadata={'mean_reward': reward, 'epoch': epoch})
     artifact.add_dir(opj(checkpoint_dir, checkpoint_name))
     run.log_artifact(artifact)
