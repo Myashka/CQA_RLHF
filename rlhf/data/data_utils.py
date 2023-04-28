7b3c7257080b3a6a0b37e06c9ad0310265db6805
Alexey Gorbatovski <porsh-5@yandex.ru>
2023-04-28 19:34:29 +0300

    Fixed question promt truncating, first commit reward validation

--- rlhf/data/data_utils.py
+++ rlhf/data/data_utils.py
@@ -10,11 +10,9 @@ def build_dataset(
     tokenizer.pad_token = tokenizer.eos_token
 
     def promt_tokenize(examples):
-        a_toks = tokenizer.encode(examples['Answer'])
-
         if data_config['truncate_promt']:
             q_toks = tokenizer.encode(examples['Question'])
-            q_toks = q_toks[:data_config['max_length_promt'] - len(a_toks)-7]
+            q_toks = q_toks[:data_config['max_length_promt']-7]
             tmp = tokenizer.decode(q_toks).strip()
         else:
             tmp = examples['Question']
@@ -25,11 +23,6 @@ def build_dataset(
             sample, padding=data_config['padding'], max_length=data_config['max_length_promt'], truncation=True)
         
         tokenized_dict['query'] = sample
-
-        # sample = torch.tensor(tokenized_dict['input_ids'] + a_toks, dtype=int)
-        # sample = sample[:data_config['max_length']]
-        # sample = tokenizer.decode(sample)
-        # tokenized_dict['question_answer'] = sample
 
         return tokenized_dict
 
